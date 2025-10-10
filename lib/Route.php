96c069b2d562f8ec274cfc296bf7c62d694ad09e
David <davigonzalezdev@gmail.com>
2025-10-10 11:55:38 -0700

    first commit

---
+++ lib/Route.php
@@ -0,0 +1,41 @@
+<?php
+ 
+namespace lib;
+
+class Route{
+
+ private static $routes = [];
+
+    public static function get($uri, $callback){
+        $uri = trim($uri, '/');
+        self::$routes['GET'][$uri] = $callback;
+    }
+
+    public static function post($uri, $callback){
+        $uri = trim($uri, '/');
+        self::$routes['POST'][$uri] = $callback;
+    }
+
+    public static function dispatch(){
+        $uri = $_SERVER['REQUEST_URI'];
+        $uri = trim($uri, '/');
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        foreach(self::$routes[$method] as $route => $callback){
+
+            if(strpos($route, ':') !== false){
+                $route = preg_replace('#:[a-zA-Z0-9]+#', '([a-zA-Z]+)', $route);
+                
+            }
+            if(preg_match("#^$route$#",$uri, $matches)){
+                echo json_encode($matches);
+                //$callback();
+                return;
+            }
+            }
+                    echo $method;
+        }
+      
+    }
+    
+
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+
+use lib\Route;
+
+Route::get('/', function(){
+    echo 'Home page';
+});
+
+Route::get('/contact', function(){
+    echo 'Contact page';
+});
+
+Route::get('/about', function(){
+    echo 'About page';
+});
+
+Route::post('/contact', function(){
+    echo 'Contact form submitted';
+});
+
+Route::get("/courses/:slug", function(){
+    echo 'Default page';
+});
+
+Route::dispatch();
